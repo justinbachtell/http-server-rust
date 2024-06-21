09295db851762982429ead988019f0d56ee7ac53
Justin Bachtell <justin@justinbachtell.com>
2024-06-20 19:36:09 -0500

    pass third stage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
-use std::io::Write;
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let request = http_request[0].split(" ").collect::<Vec<&str>>();
+    let http_response = match request[1] {
+        "/" => "200 OK",
+        _ => "404 NOT FOUND",
+    };
+
+    let response = format!("HTTP/1.1 {}\r\n\r\n", http_response);:
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
